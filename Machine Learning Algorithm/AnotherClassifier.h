e2e58e12bae4f039bdc1628a02c9364c3e37ba2a
Mromano06 <matbat2205@gmail.com>
2025-04-11 10:52:49 -0400

    Commenting changes

--- Machine Learning Algorithm/AnotherClassifier.h
+++ Machine Learning Algorithm/AnotherClassifier.h
@@ -5,8 +5,8 @@
 #include <iostream>
 
 /**
-* @class AnotherClassifier
-* @brief A placeholder class that implements the Classifier interface. Is not functional.
+/// @class AnotherClassifier
+/// @brief A placeholder class that implements the Classifier interface. Is not functional.
 *
 * This class is currently used to represent polymorphism and abstraction.
 * All functions only print that they are not implemented
@@ -14,16 +14,16 @@
 class AnotherClassifier : public Classifier {
 public:
 	/**
-	* @brief Prints a message to state that the function is not implemented
-	* @param data A vector of data samples for training (not used).
+	/// @brief Prints a message to state that the function is not implemented
+	/// @param data A vector of data samples for training (not used).
 	*
 	*/
 	void train(std::vector<DataPoint>& trainingData) override;
 
 	/**
-	* @brief Prints a message indicating prediction is not implemented.
-	* @param input A queryPoint sample to classify (ignored).
-	* @return A DataPoint.
+	/// @brief Prints a message indicating prediction is not implemented.
+	/// @param input A queryPoint sample to classify (ignored).
+	/// @return A DataPoint.
 	*
 	*/
 	DataPoint predict(DataPoint& queryPoint);
