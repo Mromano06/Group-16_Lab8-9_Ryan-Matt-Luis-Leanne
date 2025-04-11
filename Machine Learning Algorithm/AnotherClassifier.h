f0cfb8eb797785d3b53659a548e2d517c23ea813
Leanne Boulton <leanne.elizabeth@gmail.com>
2025-04-11 10:38:48 -0400

    Added Doxygen comments and functions to AnotherClassifier.h

--- Machine Learning Algorithm/AnotherClassifier.h
+++ Machine Learning Algorithm/AnotherClassifier.h
@@ -1,16 +1,33 @@
 #pragma once
+#include "Classifier.h"
+#include <string>
+#include <vector>
+#include <iostream>
 
-/// @brief 
-///
-/// 
-class AnotherClassifier 
-{
+/**
+* @class AnotherClassifier
+* @brief A placeholder class that implements the Classifier interface. Is not functional.
+*
+* This class is currently used to represent polymorphism and abstraction.
+* All functions only print that they are not implemented
+*/
+class AnotherClassifier : public Classifier {
+public:
+	/**
+	* @brief Prints a message to state that the function is not implemented
+	* @param data A vector of data samples for training (not used).
+	*
+	*/
+	void train(std::vector<DataPoint>& trainingData) override;
 
-};
+	/**
+	* @brief Prints a message indicating prediction is not implemented.
+	* @param input A queryPoint sample to classify (ignored).
+	* @return A DataPoint.
+	*
+	*/
+	DataPoint predict(DataPoint& queryPoint);
 
-/// Class Diagram (inherits from Classifier)
-///  - train()
-///  - predict()
-/// 
-/// for this class, all functionality of the given functions will only print the function name
+}
+
 
