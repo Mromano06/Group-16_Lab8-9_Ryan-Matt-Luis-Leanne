779a2943c0f53a299f84e36d02c388a5e52416d7
Ryan H. <Ryanhackbart@gmail.com>
2025-04-11 11:29:18 -0400

    doxygen update

--- Machine Learning Algorithm/AnotherClassifier.h
+++ Machine Learning Algorithm/AnotherClassifier.h
@@ -13,19 +13,15 @@
 
 class AnotherClassifier : public Classifier {
 public:
-	/**
+	
 	/// @brief Prints a message to state that the function is not implemented
 	/// @param data A vector of data samples for training (not used).
-	*
-	*/
 	void train(std::vector<DataPoint>& trainingData) override;
 
-	/**
+	
 	/// @brief Prints a message indicating prediction is not implemented.
 	/// @param input A queryPoint sample to classify (ignored).
 	/// @return A DataPoint.
-	*
-	*/
 	DataPoint predict(DataPoint& queryPoint);
 
 };
--- Machine Learning Algorithm/utils.h
+++ Machine Learning Algorithm/utils.h
@@ -3,5 +3,10 @@
 #include <iostream>
 
 // Continue Program asky
-/// @params Global function to ask to continue program
+/// @brief Global function to ask to continue program
+///
+/// asks to continut program, and continues if Y or y is entered
+/// if N or n is entered, asks if the usser is sure, as the program will exit 
+/// if the user presses Y to confirm that they pressed N, program exits
+/// if the user presses N to cancel confirmation, program continues
 void programContinue();
